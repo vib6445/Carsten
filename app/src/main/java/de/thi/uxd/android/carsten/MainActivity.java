5f4989010476b3790b56d7d90693696aab0e507c
vib6445 <vingeobrun@googlemail.com>
2019-12-04 17:08:35 +0100

    Login Commit

--- app/src/main/java/de/thi/uxd/android/carsten/MainActivity.java
+++ app/src/main/java/de/thi/uxd/android/carsten/MainActivity.java
@@ -1,8 +1,13 @@
 package de.thi.uxd.android.carsten;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +18,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        BottomNavigationView topNav = findViewById(R.id.top_navigation);
+        topNav.setOnNavigationItemSelectedListener(navListener);
 
-
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+        new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFragment = null;
+
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.nave_account:
+                        selectedFragment = new ProfileFragment();
+                        break;
+                    case R.id.nave_create:
+                        selectedFragment = new CreateFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                return true;
+            }
+        };
+
+
 }
