d4b6199b3cc7ddf5adecc51c0a508a10fa33520b
Mikele <michael.kronester@live.de>
2019-12-04 17:07:39 +0100

    du huan

--- app/src/main/java/de/thi/uxd/android/carsten/Login/LoginFragment.java
+++ app/src/main/java/de/thi/uxd/android/carsten/Login/LoginFragment.java
@@ -1,14 +1,20 @@
 package de.thi.uxd.android.carsten.Login;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import de.thi.uxd.android.carsten.MainActivity;
 import de.thi.uxd.android.carsten.R;
 
 /**
@@ -25,10 +31,28 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button loginButton = getView().findViewById(R.id.btn_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+    }
 
+    public void openMainActivity(){
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        startActivity(intent);
+    }
 
 }
