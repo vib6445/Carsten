2a220f894e25260d6981d632d0165fbbe2a9aeb7
vib6445 <vingeobrun@googlemail.com>
2019-12-05 23:10:31 +0100

    Merge branch 'Michael' of https://github.com/vib6445/Carsten into Vincent
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/de/thi/uxd/android/carsten/CreateFragment.java
+++ app/src/main/java/de/thi/uxd/android/carsten/CreateFragment.java
@@ -13,6 +13,34 @@ public class CreateFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
         return inflater.inflate((R.layout.fragment_create), container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //fixIconSize();
+    }
+
+    /* to override the standard icon size of the bottom navigation view of the Slab-Selection
+    private void fixIconSize(){
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) getActivity().findViewById(R.id.create_navigation);
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) bottomNavigationView.getChildAt(0);
+
+
+
+        for (int i = 0; i < menuView.getChildCount(); i++) {
+            final View iconView = menuView.getChildAt(i).findViewById(com.google.android.material.R.id.icon);
+            final ViewGroup.LayoutParams layoutParams = iconView.getLayoutParams();
+            final DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+            // set your height here
+            layoutParams.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 320, displayMetrics);
+            // set your width here
+            layoutParams.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 320, displayMetrics);
+            iconView.setLayoutParams(layoutParams);
+        }
+    }*/
+
 }
