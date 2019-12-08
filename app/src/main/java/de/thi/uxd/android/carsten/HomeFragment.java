dda32f6a8896ac7cba0c35edac61362a97aca976
Mikele <michael.kronester@live.de>
2019-12-08 20:44:13 +0100

    Fragment Delivery added

--- app/src/main/java/de/thi/uxd/android/carsten/HomeFragment.java
+++ app/src/main/java/de/thi/uxd/android/carsten/HomeFragment.java
@@ -4,15 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class HomeFragment extends Fragment {
+
+    Button buttonDelivery;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate((R.layout.fragment_home), container, false);
+        View v = inflater.inflate((R.layout.fragment_home), container, false);
+
+        buttonDelivery = v.findViewById(R.id.btn_deliveryStatus);
+
+        buttonDelivery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DeliveryFragment deliveryFragment = new DeliveryFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, deliveryFragment);
+                transaction.commit();
+            }
+        });
+        return v;
     }
 }
