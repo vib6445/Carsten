fc79ece954bb8c8a263eed287ea4af5d0f23feeb
vib6445 <vingeobrun@googlemail.com>
2019-12-08 13:57:24 +0100

    Added correct IDs

--- app/src/main/java/de/thi/uxd/android/carsten/CreateSlabs/LemonadeFragment.java
+++ app/src/main/java/de/thi/uxd/android/carsten/CreateSlabs/LemonadeFragment.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import de.thi.uxd.android.carsten.R;
@@ -14,6 +16,9 @@ import de.thi.uxd.android.carsten.R;
  * A simple {@link Fragment} subclass.
  */
 public class LemonadeFragment extends Fragment {
+
+    final ConstraintLayout lemonadeLayout = new ConstraintLayout(getContext());
+    final ImageView imageView = new ImageView(getContext());
 
 
     public LemonadeFragment() {
@@ -24,6 +29,7 @@ public class LemonadeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lemonade, container, false);
     }
