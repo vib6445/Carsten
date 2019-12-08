d60fe8a6487f8c6a1b0a2a6e57dd2aaf48c1e75f
vib6445 <vingeobrun@googlemail.com>
2019-12-08 14:04:16 +0100

    Merge branch 'Michael' of https://github.com/vib6445/Carsten into Vincent
    # Conflicts:
    #	app/src/main/res/layout/fragment_lemonade.xml

--- app/src/main/java/de/thi/uxd/android/carsten/CreateSlabs/LemonadeFragment.java
+++ app/src/main/java/de/thi/uxd/android/carsten/CreateSlabs/LemonadeFragment.java
@@ -5,9 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import de.thi.uxd.android.carsten.R;
@@ -16,9 +14,6 @@ import de.thi.uxd.android.carsten.R;
  * A simple {@link Fragment} subclass.
  */
 public class LemonadeFragment extends Fragment {
-
-    final ConstraintLayout lemonadeLayout = new ConstraintLayout(getContext());
-    final ImageView imageView = new ImageView(getContext());
 
 
     public LemonadeFragment() {
