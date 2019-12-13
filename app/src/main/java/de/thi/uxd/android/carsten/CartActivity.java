8e4ac3951f0205581996d5c4235f7623bf05933e
Mikele <michael.kronester@live.de>
2019-12-13 22:15:13 +0100

    Added Checkout Activity

--- app/src/main/java/de/thi/uxd/android/carsten/CartActivity.java
+++ app/src/main/java/de/thi/uxd/android/carsten/CartActivity.java
@@ -2,13 +2,33 @@ package de.thi.uxd.android.carsten;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class CartActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        Button checkoutButton = findViewById(R.id.checkoutButton);
+        checkoutButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                openCheckoutActivity();
+            }
+
+        });
+    }
+
+    public void openCheckoutActivity(){
+        Intent intent = new Intent(this, CheckoutActivity.class);
+        startActivity(intent);
     }
 }
