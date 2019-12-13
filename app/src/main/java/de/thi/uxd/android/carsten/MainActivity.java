8a767f46af5f156cd7cde3606c14e9813d9b4e97
Mikele <michael.kronester@live.de>
2019-12-13 20:44:01 +0100

    Added and linked "Your Cart" section

--- app/src/main/java/de/thi/uxd/android/carsten/MainActivity.java
+++ app/src/main/java/de/thi/uxd/android/carsten/MainActivity.java
@@ -1,7 +1,11 @@
 package de.thi.uxd.android.carsten;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,13 +21,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageButton cartButton = findViewById(R.id.cartButton);
+        cartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCartActivity();
+            }
+        });
 
         BottomNavigationView topNav = findViewById(R.id.top_navigation);
         topNav.setOnNavigationItemSelectedListener(navListener);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
 
+    }
 
+    public void openCartActivity(){
+        Intent intent = new Intent(this, CartActivity.class);
+        startActivity(intent);
     }
 
 
