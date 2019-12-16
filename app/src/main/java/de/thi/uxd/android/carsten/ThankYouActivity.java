1bc2bc63fe9b50219602b3ac23962d3975001ae6
vib6445 <vingeobrun@googlemail.com>
2019-12-16 18:54:20 +0100

    Updated Design

---
+++ app/src/main/java/de/thi/uxd/android/carsten/ThankYouActivity.java
@@ -0,0 +1,14 @@
+package de.thi.uxd.android.carsten;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThankYouActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thank_you);
+    }
+}
