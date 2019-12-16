23554c21419e66aa178306d85b673bc652bf2744
Mikele <michael.kronester@live.de>
2019-12-16 19:43:40 +0100

    Merge branch 'Vincent' of https://github.com/vib6445/carsten into Michael
    # Conflicts:
    #	app/src/debug/res/values/google_maps_api.xml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/activity_delivery.xml

--- app/src/main/java/de/thi/uxd/android/carsten/CheckoutActivity.java
+++ app/src/main/java/de/thi/uxd/android/carsten/CheckoutActivity.java
@@ -2,7 +2,10 @@ package de.thi.uxd.android.carsten;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CheckoutActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class CheckoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        Button thankYouButton = findViewById(R.id.checkoutButton);
+
+        thankYouButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openThankYouActivity();
+            }
+
+        });
+    }
+
+    public void openThankYouActivity(){
+        Intent intent = new Intent(this, ThankYouActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/de/thi/uxd/android/carsten/ThankYouActivity.java
+++ app/src/main/java/de/thi/uxd/android/carsten/ThankYouActivity.java
@@ -1,6 +1,9 @@
 package de.thi.uxd.android.carsten;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,34 @@ public class ThankYouActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thank_you);
+
+        Button deliveryButton = findViewById(R.id.quicklink_deliverystatus);
+        Button homeButton = findViewById(R.id.quicklink_home);
+
+        deliveryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDeliveryActivity();
+            }
+
+        });
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+
+        });
+    }
+
+    private void openDeliveryActivity() {
+        Intent intent = new Intent(this, DeliveryActivity.class);
+        startActivity(intent);
+    }
+
+    private void openMainActivity() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
